61d074a418c0ff83f3319cc75442ad955e389d91
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-03-23 23:56:55 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-void main(){
+void main() {
   String testing = "hello";
   print(testing);
 
@@ -11,44 +11,88 @@ void main(){
   wtf.emptyFunction();
   wtf.fix();
   wtf.exampleArguement();
+
+  // for loop:
+  lolipop pop = new lolipop();
+  pop.forLoop();
+  pop.ff();
 }
 
-class TestClass{
-  void disp(){
+class TestClass {
+  void disp() {
     print("This one is print from the  \"Class\"");
     print("uppercase".toUpperCase());
   }
 }
 
-class anotherClass{
-  void anotherFucntion(){
+class anotherClass {
+  void anotherFucntion() {
     var haha = "this is a haha String";
-    if (haha.contains("a")){
+    if (haha.contains("a")) {
       print(haha.toUpperCase() + " correct");
     } else {
       print("nope");
     }
   }
 
-  void emptyFunction(){
+  void emptyFunction() {
     int num;
     print(num);
   }
 
-  void fix(){
+  void fix() {
     const pi = 3.14;
     final area = pi * 12 * 12;
     print("The Output is ${area}, ${pi}");
   }
 
-  void exampleArguement(){
+  void exampleArguement() {
     var abc = 10;
     var a = abc > 12;
     var b = a ?? abc;
-    if (a == true){
+    if (a == true) {
       print("==");
     } else {
       print("a: ${a} \n abc: ${abc} \n b: ${b}");
     }
+  }
+}
+
+class lolipop{
+  void forLoop(){
+    // Break
+    outerloop: // This is the label name 
+    for (var i =0;  i < 5; i++ ){
+      print("innerLoop: ${i}");
+      innerloop:
+
+      for (var j = 0; j < 5; j++){
+        if (j > 3){
+          break innerloop;
+        }
+
+        if (i == 2){
+          break innerloop;
+        }
+
+        if (i == 4){
+          break outerloop;
+        }
+        print("InnerLoop: -> ${j}");
+      }
+    }
+  }
+
+  void ff(){
+    outerloop: // This is the label name
+   for (var i = 0; i < 3; i++) {
+      print("Outerloop:---> ${i}");
+      for (var j = 0; j < 5; j++) {
+         if (j == 3){
+            continue outerloop;
+         }
+         print("Innerloop:<--- ${j}");
+      }
+   }
   }
 }
